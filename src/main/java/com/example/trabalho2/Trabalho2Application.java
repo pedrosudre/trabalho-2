4a64b5bfd7279b5c600b2d5e0f6b4d91a7016b05
Pedro Sudré <pedrosudre04@gmail.com>
2023-06-20 19:25:32 -0300

    first commit

---
+++ src/main/java/com/example/trabalho2/Trabalho2Application.java
@@ -0,0 +1,13 @@
+package com.example.trabalho2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Trabalho2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Trabalho2Application.class, args);
+    }
+
+}
