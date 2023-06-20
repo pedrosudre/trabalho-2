ae87c9ea9e695182d1c9222f397ebb7703cb5df7
Pedro Sudré <pedrosudre04@gmail.com>
2023-06-20 19:40:39 -0300

    nota repository

--- src/main/java/com/example/trabalho2/repository/NotaRepository.java
+++ src/main/java/com/example/trabalho2/repository/NotaRepository.java
@@ -1,4 +1,9 @@
 package com.example.trabalho2.repository;
 
-public class NotaRepository {
+import com.example.trabalho2.model.Nota;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface NotaRepository extends MongoRepository<Nota, Integer> {
+
+    Nota save(Nota nota);
 }
